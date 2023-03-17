4ac518428e47c951e3aea26ca7685323a7fe9624
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2023-03-17 20:57:47 +0100

    gfal interface

--- downloadservice/__init__.py
+++ downloadservice/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request
 from flask_oidc import OpenIDConnect
+import gfal2
 
 app = Flask(__name__)
 
@@ -22,6 +23,48 @@ def index():
 @oidc.require_login
 def login():
     return 'Welcome %s' % oidc.user_getfield('email')
+
+
+@app.route('/list')
+# @oidc.require_login
+def list():
+    # oidc.user_getfield('email')
+    ctx = gfal2.creat_context()
+    baseurl = request.args.get("url", default="https://dcache.cta.cscs.ch:2880/pnfs/cta.cscs.ch/lst/")
+
+    try:
+        directory = ctx.opendir(baseurl)
+    except gfal2.GError as e:
+        nread = 2000
+        def generate():
+            f = ctx.open(baseurl, 'r')
+            print("opened", f)
+            while True:
+                r = f.read(nread)
+                print("read", r)
+                yield r
+                if len(r) < nread:
+                    break
+        return generate() #, {"Content-Type": "text/csv"}
+
+    urls = []
+
+    while True:
+        try:
+            (dirent, fstat) = directory.readpp()
+        except gfal2.GError as e:            
+            continue
+
+        if dirent is None or dirent.d_name is None or len(dirent.d_name) == 0:
+            break
+
+        urls.append(os.path.join(baseurl, dirent.d_name))
+
+
+    return {'delivered_for': oidc.user_getfield('email'),
+            'urls': urls}
+
+
 
 # @app.route('/')
 # def index():
