0cbf210d72f7d64849085410068e257d2333f648
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-07-20 08:02:08 +0200

    Remove certificates folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,23 +18,24 @@ __this_dir__ = os.path.join(os.path.abspath(os.path.dirname(__file__)))
 
 @pytest.fixture(scope="session")
 def app():
-    from downloadservice.app import app
+    with tempfile.TemporaryDirectory() as tmpdir:
+        from downloadservice.app import app
 
-    os.system("openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 \
-              -keyout certificates/key.pem -out certificates/cert.pem -batch")
+        os.system(f"openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 \
+                -keyout {tmpdir}/key.pem -out {tmpdir}/cert.pem -batch")
 
-    app.config.update({
-        "TESTING": True,
-        "CTADS_DISABLE_ALL_AUTH": True,
-        "DEBUG": True,
-        "CTADS_CABUNDLE": "./certificates/cert.pem",
-        "CTADS_CLIENTCERT": "./certificates/cert.pem",
-        'CTADS_UPSTREAM_ENDPOINT': 'http://127.0.0.1:31102/',
-        'CTADS_UPSTREAM_BASEPATH': '',
-        "SERVER_NAME": 'app',
-    })
+        app.config.update({
+            "TESTING": True,
+            "CTADS_DISABLE_ALL_AUTH": True,
+            "DEBUG": True,
+            "CTADS_CABUNDLE": f"{tmpdir}/cert.pem",
+            "CTADS_CLIENTCERT": f"{tmpdir}/cert.pem",
+            'CTADS_UPSTREAM_ENDPOINT': 'http://127.0.0.1:31102/',
+            'CTADS_UPSTREAM_BASEPATH': '',
+            "SERVER_NAME": 'app',
+        })
 
-    return app
+        yield app
 
 
 @contextmanager
@@ -88,80 +89,83 @@ def kill_child_processes(parent_pid, sig=signal.SIGINT):
 
 @pytest.fixture
 def start_service(pytestconfig):
+    with tempfile.TemporaryDirectory() as tmpdir:
 
-    os.system("openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 \
-              -keyout certificates/key.pem -out certificates/cert.pem -batch")
+        os.system(f"openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 \
+                -keyout {tmpdir}/key.pem -out {tmpdir}/cert.pem -batch")
 
-    rootdir = pytestconfig.rootdir
+        rootdir = pytestconfig.rootdir
 
-    env = copy.deepcopy(dict(os.environ))
-    print(("rootdir", str(rootdir)))
-    env['PYTHONPATH'] = str(rootdir) + ":" + str(rootdir) + "/tests:" + \
-        str(rootdir) + '/bin:' + \
-        __this_dir__ + ":" + os.path.join(__this_dir__, "../bin:") + \
-        env.get('PYTHONPATH', "")
+        env = copy.deepcopy(dict(os.environ))
+        print(("rootdir", str(rootdir)))
+        env['PYTHONPATH'] = str(rootdir) + ":" + str(rootdir) + "/tests:" + \
+            str(rootdir) + '/bin:' + \
+            __this_dir__ + ":" + os.path.join(__this_dir__, "../bin:") + \
+            env.get('PYTHONPATH', "")
 
-    env['CTADS_DISABLE_ALL_AUTH'] = 'True'
-    env['CTADS_CABUNDLE'] = "cabundle.pem"
-    env["CTADS_CABUNDLE"] = "./certificates/cert.pem"
-    env["CTADS_CLIENTCERT"] = "./certificates/cert.pem"
-    env['CTADS_UPSTREAM_ENDPOINT'] = 'http://127.0.0.1:31102/'
-    env['CTADS_UPSTREAM_BASEPATH'] = ''
+        env['CTADS_DISABLE_ALL_AUTH'] = 'True'
+        env['CTADS_CABUNDLE'] = "cabundle.pem"
+        env["CTADS_CABUNDLE"] = f"{tmpdir}/cert.pem"
+        env["CTADS_CLIENTCERT"] = f"{tmpdir}/cert.pem"
+        env['CTADS_UPSTREAM_ENDPOINT'] = 'http://127.0.0.1:31102/'
+        env['CTADS_UPSTREAM_BASEPATH'] = ''
 
-    print(("pythonpath", env['PYTHONPATH']))
+        print(("pythonpath", env['PYTHONPATH']))
 
-    cmd = ["python", "downloadservice/cli.py"]
+        cmd = ["python", "downloadservice/cli.py"]
 
-    print(f"\033[33mcommand: {cmd}\033[0m")
+        print(f"\033[33mcommand: {cmd}\033[0m")
 
-    p = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        shell=False,
-        env=env,
-    )
+        p = subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=False,
+            env=env,
+        )
 
-    url_store = [None]
+        url_store = [None]
 
-    def follow_output():
-        url_store[0] = None
-        for line in iter(p.stdout):
-            line = line.decode()
+        def follow_output():
+            url_store[0] = None
+            for line in iter(p.stdout):
+                line = line.decode()
 
-            NC = '\033[0m'
-            if 'ERROR' in line:
-                C = '\033[31m'
-            else:
-                C = '\033[34m'
+                NC = '\033[0m'
+                if 'ERROR' in line:
+                    C = '\033[31m'
+                else:
+                    C = '\033[34m'
 
-            print(f"{C}following server: {line.rstrip()}{NC}")
-            m = re.search(r"Running on (.*?:5000)", line)
-            if m:
-                # alternatively get from configenv
-                url_store[0] = m.group(1).strip()
-                print(f"{C}following server: found url:{url_store[0]}")
+                print(f"{C}following server: {line.rstrip()}{NC}")
+                m = re.search(r"Running on (.*?:5000)", line)
+                if m:
+                    # alternatively get from configenv
+                    url_store[0] = m.group(1).strip()
+                    print(f"{C}following server: found url:{url_store[0]}")
 
-            if re.search("\* Debugger PIN:.*?", line):
-                url_store[0] = url_store[0].replace("0.0.0.0", "127.0.0.1")
-                print(f"{C}following server: server ready, url {url_store[0]}")
+                if re.search(r"\* Debugger PIN:.*?", line):
+                    url_store[0] = url_store[0].replace("0.0.0.0", "127.0.0.1")
+                    print(f"{C}following server: ready, url {url_store[0]}")
 
-    thread = Thread(target=follow_output, args=())
-    thread.start()
+        thread = Thread(target=follow_output, args=())
+        thread.start()
 
-    started_waiting = time.time()
-    while url_store[0] is None:
-        print("waiting for server to start since",
-              time.time() - started_waiting)
-        time.sleep(0.2)
-    time.sleep(0.5)
+        started_waiting = time.time()
+        while url_store[0] is None:
+            print("waiting for server to start since",
+                  time.time() - started_waiting)
+            time.sleep(0.2)
+        time.sleep(0.5)
 
-    service = url_store[0]
+        service = url_store[0]
 
-    yield dict(
-        url=service,
-        pid=p.pid
-    )
+        yield dict(
+            url=service,
+            pid=p.pid
+        )
 
-    kill_child_processes(p.pid, signal.SIGINT)
-    os.kill(p.pid, signal.SIGINT)
+        # TODO: Clean certificates directly
+
+        kill_child_processes(p.pid, signal.SIGINT)
+        os.kill(p.pid, signal.SIGINT)
