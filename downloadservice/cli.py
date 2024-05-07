ffceb6c1da8fd5b87d1aa1ac1626921b7f9f0b88
bastien wermeille <bastien.wermeille@cta-consortium.org>
2024-05-07 10:01:43 +0200

    production server

--- downloadservice/cli.py
+++ downloadservice/cli.py
@@ -1,11 +1,12 @@
 import logging
 
 from downloadservice.app import app
+from waitress import serve
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    app.run(host='0.0.0.0', port=5000)
+    serve(app, host="0.0.0.0", port=5000)
 
 
 if __name__ == "__main__":
