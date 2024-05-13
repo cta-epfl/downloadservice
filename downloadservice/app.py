a5508493f27a6b45220e450ef30a762dad01eae4
bastien wermeille <bastien.wermeille@cta-consortium.org>
2024-05-13 10:22:08 +0200

    Fix lint issue

--- downloadservice/app.py
+++ downloadservice/app.py
@@ -167,9 +167,10 @@ def authenticated(f):
             else:
                 user = None
 
+            content_type = request.headers.get("Content-Type")
             if user:
                 return f(user, *args, **kwargs)
-            elif request.headers.get("Content-Type").lower() == "application/json":
+            elif content_type and content_type.lower() == "application/json":
                 return {'message': 'Invalid or missing Bearer token'}, 401
             else:
                 # redirect to login url on failed auth
